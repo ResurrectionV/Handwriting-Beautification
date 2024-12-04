ed40c9f7bb8561e2d3a956d1871c70ef02a41855
Ernie Chu <schu23@jhu.edu>
2024-12-04 11:06:40 -0500

    add inversion interpolate

--- scheduling_rectflow.py
+++ scheduling_rectflow.py
@@ -37,3 +37,28 @@ class RectFlowScheduler(DDPMScheduler):
         t = timesteps.view(-1, *([1] * (noise.ndim - 1))) / self.config.num_train_timesteps
         return t * noise + (1.-t) * original_samples
 
+class RectFlowInverseScheduler(RectFlowScheduler):
+    """
+    RectFlowInverseScheduler is the reverse scheduler of [`RectFlowScheduler`].
+    """
+    def step(
+        self,
+        model_output: torch.Tensor,
+        timestep: int,
+        sample: torch.Tensor,
+        generator=None,
+        return_dict: bool = True,
+    ) -> Union[DDPMSchedulerOutput, Tuple]:
+
+        ### NOTE: Use Euler method to sample from the learned flow
+        dt = 1. / self.num_inference_steps
+        pred_prev_sample = sample - model_output * dt
+        pred_original_sample = sample - model_output * (timestep * dt)
+
+        if not return_dict:
+            return (
+                pred_prev_sample,
+                pred_original_sample,
+            )
+
+        return DDPMSchedulerOutput(prev_sample=pred_prev_sample, pred_original_sample=pred_original_sample)
